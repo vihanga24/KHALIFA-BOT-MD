89a43f1069a8014e26cc29bd540ea7537e783abc
vihanga24 <85000887+vihanga24@users.noreply.github.com>
2024-02-03 11:22:37 +0530

    Add files via upload

---
+++ temp/autovn.js
@@ -0,0 +1,8 @@
+//KETIKA ADA YANG TOXIC
+exports.vnToxic = [
+//Jangan toxic pm
+"https://cdn.filestackcontent.com/Zdfunz5gQLeTCF4DlQOW",
+//Heeh
+"https://cdn.filestackcontent.com/YYG9wdIgRLSHi5Or45dO"
+]
+exports.vnMenu = ["https://sf16-ies-music-va.tiktokcdn.com/obj/musically-maliva-obj/7321048497540188934.mp3"]
---
+++ temp/stickerPack.js
@@ -0,0 +1,9 @@
+const fs = require('fs')
+
+
+exports.otw = fs.readFileSync('./temp/sticker/oke tunggu.webp')
+exports.bad = fs.readFileSync('./temp/sticker/istighfar.webp')
+exports.marah = fs.readFileSync('./temp/sticker/marah.webp')
+exports.eror = fs.readFileSync('./temp/sticker/error.webp')
+exports.paan = fs.readFileSync('./temp/sticker/why.webp')
+exports.fuck = fs.readFileSync('./temp/sticker/pakyu.webp')
